ea422c46ec6fb38571baec2fd9fe0116d3d01a2b
SimranDhillon20 <46889015+SimranDhillon20@users.noreply.github.com>
2020-02-26 01:24:18 +0000

    RayCastWeapons 4/5 done

---
+++ RayCastWeapons/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    public Weapon[] weapons;
+    int activeWeaponIndex = -1;
+    Weapon activeWeapon;
+	void Start ()
+    {
+        SetActiveWeapon(0);
+	}
+	
+	void Update ()
+    {
+        HandleWeaponSwitching();
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (activeWeapon)
+            {
+                activeWeapon.Fire(transform.position);
+
+            }
+        }
+       
+
+		
+	}
+
+
+
+    private void HandleWeaponSwitching()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetActiveWeapon(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetActiveWeapon(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SetActiveWeapon(2);
+        }
+
+
+
+    }
+
+    private void SetActiveWeapon(int index)
+    {
+        if (index >= 0 && index <= weapons.Length)
+        {
+            if (activeWeapon)
+            {
+                Destroy(activeWeapon.gameObject);
+            }
+
+                activeWeapon = Instantiate(weapons[index], transform);
+                activeWeaponIndex = index;
+            
+        }
+    }
+}
---
+++ RayCastWeapons/Assets/Scripts/RayCastWeapon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastWeapon : Weapon
+{
+
+    protected RaycastHit raycastHit;
+    public float Range;
+    public float DamagePerHit;
+
+    protected void ApplyDamage(HealthComponent healthComponent)
+    {
+        healthComponent.ApplyDamage(DamagePerHit);
+    }
+	
+}
